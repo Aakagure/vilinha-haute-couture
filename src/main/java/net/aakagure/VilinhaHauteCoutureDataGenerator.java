8873434ac93f961553cf0f782172873977ab6a4e
Aakagure <hugo.cardoso.freitas@gmail.com>
2026-01-18 00:08:31 -0300

    Initial commit

---
+++ src/main/java/net/aakagure/VilinhaHauteCoutureDataGenerator.java
@@ -0,0 +1,11 @@
+package net.aakagure;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class VilinhaHauteCoutureDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
